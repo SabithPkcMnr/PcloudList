d6b87298bc8f8d8148bd30228da8ff64f78f7de5
Sabith Pkc Mnr <sabithpkcmnr@gmail.com>
2021-05-05 22:52:32 +0530

    Oh shit, here we go again :)

---
+++ app/src/main/java/com/sabithpkcmnr/pcloudlist/ActivityHome.java
@@ -0,0 +1,18 @@
+package com.sabithpkcmnr.pcloudlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+
+    }
+}
